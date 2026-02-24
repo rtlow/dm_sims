86a777e01d9a78fc82f1a1710adc5b62d9dca0cf
rtlow <37086221+rtlow@users.noreply.github.com>
2026-02-24 16:04:14 -0600

    trying to study quenching

--- sh/get-spectra-mocks.py
+++ sh/get-spectra-mocks.py
@@ -2,12 +2,11 @@ import glob
 import sys
 
 from fake_spectra.randspectra import RandSpectra
-from mpi4py import MPI
 
 IN_PATH = sys.argv[1]
 
 for i in range(len(glob.glob(f"{IN_PATH}/snap*"))):
-    rr = RandSpectra(i, IN_PATH, MPI=MPI, thresh=0.)
+    rr = RandSpectra(i, IN_PATH, thresh=0.)
     rr.get_tau("H",1,1215)
     #Lyman-beta
     rr.get_tau("H",1,1025)
